854a5794e02ebbd61fe4c34b9db3a38611c1bb4d
Apiclo <u.apo@outlook.com>
2024-06-16 11:41:57 +0800

    自动化脚本
    Linux可以执行run.sh一键执行,revue.sh可以一键刷新正在运行的springboot生产环境的静态资源

---
+++ revue.sh
@@ -0,0 +1,19 @@
+#/bin/zsh
+#此项目采用单页面路由构建
+#用来自动构建vite build然后刷新到target静态资源
+#可以实现不停止springboot的同时完成vite静态资源刷新
+#022201230237 王超磊
+#仅适用于Linux系统
+# rm -rf target
+cd vite
+# npm install
+npm run build
+echo "vite构建完毕"
+cd ../
+rm -rf src/main/resources/static
+rm -rf target/classes/static
+echo "旧文件清理完毕"
+cp -r vite/dist src/main/resources/static
+cp -r vite/dist target/classes/static
+rm -rf vite/dist
+echo "静态资源刷新完毕"
---
+++ run.sh
@@ -0,0 +1,26 @@
+#/bin/zsh
+#022201230237 王超磊
+#自动编译并运行springboot
+#自动替换vite静态资源
+#仅适用于Linux系统
+
+rm -rf target
+./mvnw clean -f pom.xml
+echo "已清理完毕"
+./mvnw install -f pom.xml
+./mvnw compile -f pom.xml
+echo "maven 命令执行完毕"
+cd vite
+npm install
+npm run build
+echo "vite静态资源构建完毕"
+cd ../
+rm -rf src/main/resources/static
+rm -rf target/classes/static
+cp -r vite/dist src/main/resources/static
+cp -r vite/dist target/classes/static
+rm -rf vite/dist
+echo "vite静态资源替换完毕"
+
+./mvnw spring-boot:run
+echo "已终止运行"
