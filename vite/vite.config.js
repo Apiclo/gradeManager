d0f196cb8644d0c1bbd9032c5336670560ab8f85
Apiclo <55468918+Apiclo@users.noreply.github.com>
2024-06-17 00:03:35 +0800

    细节调整

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-//022201230237王超磊
+//022201230237wcl
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
